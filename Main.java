3cba56ee327f92ee832504067bf9ddd17aa0eca7
Minz16 <38834696+Minz16@users.noreply.github.com>
2023-10-09 19:27:31 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,8 +5,14 @@ public class Main {
         int[] nums = {2,54,34,2,5,78,6,3,143,1,2,3,4,5,6,7,8,9,10};
         System.out.println(Arrays.toString(maxSort(nums)));
     }
+    public static void mergeSort() {
+        //Average-Case: O(n log(n))
+    }
 
-    public static void lineareSuche(int[] nums, int suche) {
+        public static void lineareSuche(int[] nums, int suche) {
+        //Average-Case: O(log(n))
+        //Worst-Case: O(log(n))
+        //Best-Case: O(1)
         for (int i = 0; i<nums.length; i++) {
             if(nums[i]==suche) {
                 System.out.println("Zahl bei Index " + i);
@@ -15,6 +21,9 @@ public class Main {
     }
 
     public static void binareSuche(int[] nums, int suche) {
+        //Average-Case: O(n)
+        //Worst-Case: O(n)
+        //Best-Case: O(1)
         int links = 0;
         int rechts = nums.length-1;
         int zahl = -1;
@@ -40,6 +49,7 @@ public class Main {
     }
 
     public static int[] minSort(int[] nums) {
+        //Average-Case: O(n²)
         for (int i=0; i<nums.length; i++) {
             int min = i;                            //Definiere i als kleinste Stelle
             for (int j=i; j<nums.length; j++) {     //Wiederhole für alle Elemente, außer die Kleinsten
@@ -56,6 +66,7 @@ public class Main {
     }
 
     public static int[] maxSort(int[] nums) {
+        //Average-Case: O(n²)
         for (int i=nums.length-1; i>=0; i--) {
             int max=i;
             for (int j=i; j>=0; j--) {
@@ -73,6 +84,7 @@ public class Main {
     }
 
     public static int[] maxToMinSort(int[] nums) {
+        //Average-Case: O(n²)
         for (int i=0; i<nums.length; i++) {
             int max=i;
             for (int j=i; j< nums.length; j++) {
@@ -89,6 +101,7 @@ public class Main {
     }
 
     public static int[] bubbleSort(int[] nums) {
+        //Average-Case: O(n²)
         for (int i=1; i<nums.length; i++) {
             for (int j=0; j<nums.length-i; j++) {
                 if (nums[j]>nums[j+1]) {
